aa55c9b6fb2e6f4d511ad459c7b7f918f84837e4
amysews <amyc271@gmail.com>
2018-01-17 12:49:21 +0000

    Display list of users on UsersPage

--- nc-news-app/src/components/UsersPage.js
+++ nc-news-app/src/components/UsersPage.js
@@ -2,16 +2,41 @@ import React from 'react';
 
 class UsersPage extends React.Component {
   state = {
-
+    users: []
   };
 
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/users`)
+      .then(buffer => buffer.json())
+      .then(({ users }) => this.setState({ users }))
+      .catch(console.log)
+  }
+
   render() {
+    const { users } = this.state;
     return (
       <section>
-        <h1>Users Info</h1>
+        <h1>List of Users</h1>
+        {users.map((user, i) => {
+          return (
+            <UserSummary user={user} />
+          )
+        })}
       </section>
     );
   }
+}
+
+const UserSummary = ({ user }) => {
+  return (
+    <section>
+      <span>Username: {user.username}</span>  <span>Name: {user.name}</span>
+    </section>
+  )
 }
 
 export default UsersPage;
