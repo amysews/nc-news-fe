48dc3e091fef533f4483a6ae6bdf618a602c8642
amysews <amyc271@gmail.com>
2018-01-17 14:15:37 +0000

    Add list of topics to TopicsPage

--- nc-news-app/src/components/TopicsPage.js
+++ nc-news-app/src/components/TopicsPage.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class TopicsPage extends React.Component {
   state = {
-
+    topics: []
   };
 
+  componentDidMount() {
+    this.fetchTopics();
+  }
+
+  fetchTopics = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/topics`)
+      .then(buffer => buffer.json())
+      .then(({ topics }) => this.setState({ topics }))
+      .catch(console.log)
+  }
+
   render() {
+    const { topics } = this.state;
     return (
       <section>
         <h1>List of Topics</h1>
-        <p>Topic A</p>
-        <p>Topic B</p>
-        <p>Topic C</p>
+        {topics.map((topic, i) => {
+          return (
+            <Topic topic={topic} key={i} />
+          )
+        })}
       </section>
     );
   }
+}
+
+const Topic = ({ topic }) => {
+  return (
+    <section>
+      <Link to={'/topics/' + topic.slug}><p>{topic.title}</p></Link>
+    </section>
+  )
 }
 
 export default TopicsPage;
